78b2ec59d2feb8a5ebb5832d949f7db121d25b1b
Keith Ma <keithchezzy@gmail.com>
2022-12-30 11:04:52 -0800

    search

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,6 +1,21 @@
 function findShortestStringRecursive(arr) {
+  if (arr.length === 1){
+    return arr[0]
+  }
+  if (arr[0].length < arr[1].length){
+    return arr[0]
+  } else {
+    return arr[1]
+  }
+  findShortestStringRecursive([arr[1]].concat(findShortestStringRecursive(arr.slice(0))))
+
+  // iterate way
+  // return arr.reduce((shortest,string)=>
+  // string.length < shortest.length ? string : shortest
+  // )
   // type your code here
 }
+
 
 if (require.main === module) {
   // add your own tests in here
